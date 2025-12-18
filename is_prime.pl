90a14be575dd0b82abca9a4b2a65455711e9a800
gavriel.cohen@live.biu.ac.il <gavriel.cohen@live.biu.ac.il>
2025-12-18 10:24:03 +0200

    start ass3

--- is_prime.pl
+++
@@ -1,7 +0,0 @@
-check_divisor(X, Y) :- Y * Y > X, !.
-check_divisor(X, Y) :- 0 is X mod Y, !, fail.
-check_divisor(X, Y) :- Y1 is Y + 1, check_divisor(X, Y1).
-
-is_prime(1) :- !, fail.
-is_prime(2) :- !.
-is_prime(X) :- X > 2, check_divisor(X, 2).
--- remove_at.pl
+++
@@ -1,3 +0,0 @@
-remove_at(X, [X|T], 1, T).
-remove_at(X, [H|T], K, [H|R]) :- K > 1, J is K - 1, remove_at(X, T, J, R).
-
